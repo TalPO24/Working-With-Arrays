08435def488d4abcdd609966d99c99a42ba7c453
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-16 19:34:49 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -80,10 +80,29 @@ displayMovments(account1.movements)
 
 const calcDisplayBalance = function(movements) {
     const balance = movements.reduce((acc, mov) => acc + mov, 0)
-    labelBalance.textContent = `${balance} EUR`
+    labelBalance.textContent = `${balance}€ `
 }
 calcDisplayBalance(account1.movements)
 
+const calcDisplaySummary = function(movements) {
+    const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0)
+    labelSumIn.textContent = `${incomes}€ `
+
+    const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
+    labelSumOut.textContent = `${Math.abs(out)}€`
+
+    const interest = movements
+        .filter(mov => mov > 0)
+        .map(deposit => deposit * 1.2 / 100)
+        .filter((int, i, arr) => {
+            console.log(arr)
+            return int >= 1
+        })
+        .reduce((acc, int) => acc + int, 0)
+    labelSumInterest.textContent = `${interest}€ `
+
+}
+calcDisplaySummary(account1.movements)
 
 //* Cumputing UserNames
 const createUsernames = function(accs) {
@@ -313,7 +332,7 @@ const max = movements.reduce((acc, mov) => {
 }, movements[0])
 console.log(max)
 */
-
+/*
 //* Challeng2 #2
 const calcAverageHumanAge = function(ages) {
     const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
@@ -324,3 +343,17 @@ const calcAverageHumanAge = function(ages) {
 const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
 const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
 console.log(average1, average2)
+*/
+
+//* The Magic of Chaining Methods
+// PIPELINE 
+const eurToUsd = 1.1
+console.log(movements)
+const totalDepositsUSD = movements.filter(mov => mov > 0)
+    .map((mov, i, arr) => {
+        // console.log(arr)
+        return mov * eurToUsd
+    })
+    // .map(mov => mov * eurToUsd)
+    .reduce((acc, mov) => acc + mov, 0)
+console.log(totalDepositsUSD)
