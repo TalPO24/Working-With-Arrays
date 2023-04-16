2d11f76704fc0d1e37001af04e867edf2fb0ca0e
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-16 19:52:39 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const calcDisplaySummary = function(movements) {
         .filter(mov => mov > 0)
         .map(deposit => deposit * 1.2 / 100)
         .filter((int, i, arr) => {
-            console.log(arr)
+            // console.log(arr)
             return int >= 1
         })
         .reduce((acc, int) => acc + int, 0)
@@ -345,6 +345,7 @@ const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
 console.log(average1, average2)
 */
 
+/*
 //* The Magic of Chaining Methods
 // PIPELINE 
 const eurToUsd = 1.1
@@ -357,3 +358,14 @@ const totalDepositsUSD = movements.filter(mov => mov > 0)
     // .map(mov => mov * eurToUsd)
     .reduce((acc, mov) => acc + mov, 0)
 console.log(totalDepositsUSD)
+*/
+
+//* Challenge #3
+const calcAverageHumanAge = ages =>
+    ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+
+const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+console.log(average1, average2)
