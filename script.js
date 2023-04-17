9ba1a3a161a4fee2126712e945a50621a199682b
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-17 19:30:32 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -76,24 +76,25 @@ const displayMovments = function(movements) {
         containerMovements.insertAdjacentHTML('afterbegin', html)
     })
 }
-displayMovments(account1.movements)
+
 
 const calcDisplayBalance = function(movements) {
     const balance = movements.reduce((acc, mov) => acc + mov, 0)
     labelBalance.textContent = `${balance}€ `
 }
-calcDisplayBalance(account1.movements)
 
-const calcDisplaySummary = function(movements) {
-    const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0)
+const calcDisplaySummary = function(acc) {
+    const incomes = acc.movements
+        .filter(mov => mov > 0)
+        .reduce((acc, mov) => acc + mov, 0)
     labelSumIn.textContent = `${incomes}€ `
 
-    const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
+    const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
     labelSumOut.textContent = `${Math.abs(out)}€`
 
-    const interest = movements
+    const interest = acc.movements
         .filter(mov => mov > 0)
-        .map(deposit => deposit * 1.2 / 100)
+        .map(deposit => (deposit * acc.interestRate) / 100)
         .filter((int, i, arr) => {
             // console.log(arr)
             return int >= 1
@@ -102,7 +103,6 @@ const calcDisplaySummary = function(movements) {
     labelSumInterest.textContent = `${interest}€ `
 
 }
-calcDisplaySummary(account1.movements)
 
 //* Cumputing UserNames
 const createUsernames = function(accs) {
@@ -116,7 +116,31 @@ const createUsernames = function(accs) {
 };
 createUsernames(accounts);
 
+//* Implementing Login
+//Event handlers
+let currentAccount;
+btnLogin.addEventListener('click', function(e) {
+    // Prevent form from submitting
+    e.preventDefault()
 
+    currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
+    console.log(currentAccount)
+
+    if (currentAccount && currentAccount.pin === Number(inputLoginPin.value)) {
+        // Display UI and message
+        labelWelcome.textContent = `Welcome Back, ${currentAccount.owner.split(' ')[0]}`
+        containerApp.style.opacity = 100
+            // Clear input fields
+        inputLoginUsername.value = inputLoginPin.value = '';
+        inputLoginPin.blur()
+            // Display movments
+        displayMovments(currentAccount.movements)
+            // Display balance
+        calcDisplayBalance(currentAccount.movements)
+            // Display summary
+        calcDisplaySummary(currentAccount)
+    }
+})
 
 
 /*
@@ -371,6 +395,7 @@ const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
 console.log(average1, average2)
 */
 
+/*
 //* The Find Method
 const firstWithdrawal = movements.find(mov => mov < 0)
 console.log(movements)
@@ -380,3 +405,4 @@ console.log(accounts)
 
 const account = accounts.find(acc => acc.owner === 'Jessica Davis')
 console.log(account)
+*/
