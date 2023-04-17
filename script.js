2deda8be24d2c5e86876e30630cd1845cfa14442
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-17 20:08:21 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -78,9 +78,9 @@ const displayMovments = function(movements) {
 }
 
 
-const calcDisplayBalance = function(movements) {
-    const balance = movements.reduce((acc, mov) => acc + mov, 0)
-    labelBalance.textContent = `${balance}€ `
+const calcDisplayBalance = function(acc) {
+    acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0)
+    labelBalance.textContent = `${acc.balance}€ `
 }
 
 const calcDisplaySummary = function(acc) {
@@ -116,6 +116,16 @@ const createUsernames = function(accs) {
 };
 createUsernames(accounts);
 
+const updateUI = function(acc) {
+    // Display movments
+    displayMovments(acc.movements)
+        // Display balance
+    calcDisplayBalance(acc)
+        // Display summary
+    calcDisplaySummary(acc)
+}
+
+
 //* Implementing Login
 //Event handlers
 let currentAccount;
@@ -133,12 +143,24 @@ btnLogin.addEventListener('click', function(e) {
             // Clear input fields
         inputLoginUsername.value = inputLoginPin.value = '';
         inputLoginPin.blur()
-            // Display movments
-        displayMovments(currentAccount.movements)
-            // Display balance
-        calcDisplayBalance(currentAccount.movements)
-            // Display summary
-        calcDisplaySummary(currentAccount)
+            // Update UI
+        updateUI(currentAccount)
+    }
+})
+
+btnTransfer.addEventListener('click', function(e) {
+    e.preventDefault()
+    const amount = Number(inputTransferAmount.value)
+    const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value)
+    inputTransferAmount.value = inputTransferTo.value = ''
+
+
+    if (amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc.username !== currentAccount.username) {
+        // Doing the transfer
+        currentAccount.movements.push(-amount);
+        receiverAcc.movements.push(amount);
+        // Update UI
+        updateUI(currentAccount)
     }
 })
 
