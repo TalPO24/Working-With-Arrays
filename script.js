4ae01dd67672662cf8e57ee3786dde92f6f491d1
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-18 11:14:23 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -164,6 +164,25 @@ btnTransfer.addEventListener('click', function(e) {
     }
 })
 
+//* The FindIndex method
+btnClose.addEventListener('click', function(e) {
+    e.preventDefault()
+
+    if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+        const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+        console.log(index)
+
+        // Delete account 
+        accounts.splice(index, 1)
+
+        // Hide UI 
+        containerApp.style.opacity = 0
+
+    } else {
+        console.log('not correct')
+    }
+    inputCloseUsername.value = inputClosePin.value = ''
+})
 
 /*
 //* Challenge #1
