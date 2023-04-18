f386a48b3a1d40bcb5a4e15e0153f19ae4df148e
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-18 20:45:41 +0300

    Working With Arrays

--- script.js
+++ script.js
@@ -527,7 +527,7 @@ console.log(arrDeep.flat(2)) // 2 level deep
 const overallBalacne = accounts.map(acc => acc.movements).flat().reduce((acc, mov) => acc + mov, 0)
 console.log(overallBalacne)
 
-// The flatMap() method returns a new array formed by applying a given callback function to each element of the array, and then flattening the result by one level
+The flatMap() method returns a new array formed by applying a given callback function to each element of the array, and then flattening the result by one level
 const overallBalacne2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov) => acc + mov, 0)
 console.log(overallBalacne2)
 */
@@ -568,8 +568,8 @@ movements.sort((a, b) => b - a)
 console.log(movements)
 */
 
+/*
 //* More Ways of Creatin and Filling Arrays
-
 const arr = [1, 2, 3, 4, 5, 6, 7]
 console.log(new Array(1, 2, 3, 4, 5, 6, 7))
 
@@ -605,3 +605,47 @@ labelBalance.addEventListener('click', function() {
     )
     console.log(movementsUI)
 })
+*/
+
+//* Array Methods Practice
+
+// 1.
+const bankDepositSum = accounts.flatMap(acc => acc.movements).filter(mov => mov > 0).reduce((acc, i, arr) => acc + i, 0)
+console.log('1.', bankDepositSum)
+
+//2.
+// const numDeposits1000 = accounts.flatMap(acc => acc.movements).filter(acc => acc >= 1000).length
+// console.log(numDeposits1000)
+
+const numDeposits1000 = accounts.flatMap(acc => acc.movements)
+    // .reduce((count, cur) => cur >= 1000 ? count + 1 : count, 0)
+    .reduce((count, cur) => cur >= 1000 ? ++count : count, 0)
+console.log('2.', numDeposits1000)
+
+// Prefixed ++ operator
+let a = 10
+console.log(++a)
+console.log(a)
+
+// 3.
+const { deposits, withdrawals } = accounts
+    .flatMap(acc => acc.movements)
+    .reduce((sums, cur) => {
+        // cur > 0 ? sums.deposits += cur : sums.withdrawals += cur;
+        sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur
+        return sums
+    }, { deposits: 0, withdrawals: 0 })
+console.log('3.', deposits, withdrawals)
+
+// 4. 
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function(title) {
+    const capitalize = str => str[0].toUpperCase() + str.slice(1)
+
+    const exceptions = ['a', 'an', 'but', 'or', 'and', 'on', 'in', 'with']
+    const titleCase = title.toLowerCase().split(' ').map(word => exceptions.includes(word) ? word : capitalize(word)).join(' ')
+    return capitalize(titleCase)
+}
+console.log(convertTitleCase('this is a nice title'))
+console.log(convertTitleCase('this is a LONG title but not too long'))
+console.log(convertTitleCase('and here is another title with an EXAMPLE'))
